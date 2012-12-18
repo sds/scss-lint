f0e5a50caf32c7f376658841d47ada86cc35e512
Shane da Silva <shane@causes.com>
2012-12-17 22:59:42 -0800

    Add property declaration format linter
    Adds a linter which ensures that all property declarations have the
    form:
    
        property-name: value;
    
    That is, it is on one line and contains the property name, followed by a
    colon, followed by a space, the expression, and a semi-colon to end the
    declaration.
    
    Change-Id: I7bf58eb0e0f46f1f94d35d5c18e730ff3d108663
    Reviewed-on: https://gerrit.causes.com/16563
    Reviewed-by: Greg Hurrell <greg@causes.com>
    Tested-by: Shane da Silva <shane@causes.com>

---
+++ lib/scss_lint/linter/property_format_linter.rb
@@ -0,0 +1,30 @@
+require 'sass'
+
+module SCSSLint
+  class Linter::PropertyFormatLinter < Linter
+    include LinterRegistry
+
+    class << self
+      def run(engine)
+        lints = []
+        engine.tree.each do |node|
+          if node.is_a?(Sass::Tree::PropNode)
+            lints << check_property_format(node, engine.lines[node.line - 1]) if node.line
+          end
+        end
+        lints.compact
+      end
+
+      def description
+        'Property declarations should always be on one line of the form ' <<
+        '`property-name: value;`'
+      end
+
+    private
+
+      def check_property_format(prop_node, line)
+        return create_lint(prop_node) unless line =~ /^\s*[\w-]+: [^\s][^;]*;/
+      end
+    end
+  end
+end
---
+++ spec/linter/property_format_linter_spec.rb
@@ -0,0 +1,144 @@
+require 'spec_helper'
+
+describe SCSSLint::Linter::PropertyFormatLinter do
+  let(:engine) { SCSSLint::Engine.new(css) }
+  subject { SCSSLint::Linter::PropertyFormatLinter.run(engine) }
+
+  context 'when rule is empty' do
+    let(:css) { <<-EOS }
+      p {
+      }
+    EOS
+
+    it 'returns no lints' do
+      subject.should be_empty
+    end
+  end
+
+  context 'when rule contains properties with spaces after the colon' do
+    let(:css) { <<-EOS }
+      p {
+        background: #000;
+        margin: 5px;
+      }
+    EOS
+
+    it 'returns no lints' do
+      subject.should be_empty
+    end
+  end
+
+  context 'when a valid property declaration is followed by a comment' do
+    let(:css) { <<-EOS }
+      p {
+        background: #000; // This is a comment
+        color: #fff; /* This is another comment */
+      }
+    EOS
+
+    it 'returns no lints' do
+      subject.should be_empty
+    end
+  end
+
+  context 'when a property declaration does not have a space after the colon' do
+    let(:css) { <<-EOS }
+      p {
+        color:#fff;
+      }
+    EOS
+
+    it 'returns a lint' do
+      subject.count.should == 1
+    end
+
+    it 'returns the correct line for the lint' do
+      subject.first.line.should == 2
+    end
+  end
+
+  context 'when a property declaration has a space before the colon' do
+    let(:css) { <<-EOS }
+      p {
+        color : #fff;
+      }
+    EOS
+
+    it 'returns a lint' do
+      subject.count.should == 1
+    end
+
+    it 'returns the correct line for the lint' do
+      subject.first.line.should == 2
+    end
+  end
+
+  context 'when a property declaration has more than one space after the colon' do
+    let(:css) { <<-EOS }
+      p {
+        color:  #fff;
+      }
+    EOS
+
+    it 'returns a lint' do
+      subject.count.should == 1
+    end
+
+    it 'returns the correct line for the lint' do
+      subject.first.line.should == 2
+    end
+  end
+
+  context 'when a property declaration spans two lines' do
+    let(:css) { <<-EOS }
+      p {
+        color:
+          #f00;
+      }
+    EOS
+
+    it 'returns a lint' do
+      subject.count.should == 1
+    end
+
+    it 'returns the correct line for the lint' do
+      subject.first.line.should == 3
+    end
+  end
+
+  context 'when a property declaration has no space in a nested rule' do
+    let(:css) { <<-EOS }
+      p {
+        color: #000;
+        a {
+          color:#f00;
+        }
+      }
+    EOS
+
+    it 'returns a lint' do
+      subject.count.should == 1
+    end
+
+    it 'returns the correct line for the lint' do
+      subject.first.line.should == 4
+    end
+  end
+
+  context 'when a property declaration has no semi-colon ending it' do
+    let(:css) { <<-EOS }
+      p {
+        color: #000;
+        background: #fff
+      }
+    EOS
+
+    it 'returns a lint' do
+      subject.count.should == 1
+    end
+
+    it 'returns the correct line for the lint' do
+      subject.first.line.should == 4
+    end
+  end
+end
