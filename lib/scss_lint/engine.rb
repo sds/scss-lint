9264518a3b73f9ee53d5035934e09880ee456be0
Shane da Silva <shane@causes.com>
2012-12-17 16:06:59 -0800

    Store actual string contents of SCSS files
    This will enable us to write linters that can access the original file
    in cases where line number or other such information can be inferred
    separately.
    
    Change-Id: I02182d1fdd19f1ce1a4c4e072c7a0655f1732889
    Reviewed-on: https://gerrit.causes.com/16557
    Reviewed-by: Shane da Silva <shane@causes.com>
    Tested-by: Shane da Silva <shane@causes.com>
    Reviewed-by: Greg Hurrell <greg@causes.com>

--- lib/scss_lint/engine.rb
+++ lib/scss_lint/engine.rb
@@ -4,11 +4,15 @@ module SCSSLint
   class Engine
     ENGINE_OPTIONS = { cache: false, syntax: :scss }
 
+    attr_reader :contents
+
     def initialize(scss_or_filename)
       if File.exists?(scss_or_filename)
         @engine = Sass::Engine.for_file(scss_or_filename, ENGINE_OPTIONS)
+        @contents = File.open(scss_or_filename, 'r').read
       else
         @engine = Sass::Engine.new(scss_or_filename, ENGINE_OPTIONS)
+        @contents = scss_or_filename
       end
     end
 
