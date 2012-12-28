84202ef22551ef42087ba9229b57c71d6f46fba4
Joe Lencioni <joe.lencioni@causes.com>
2012-12-28 13:25:43 -0800

    Teach scss-lint to detect too-long shorthand values
    With this commit, the linter now complains about styles that use
    shorthand versions that can be further shortened, such as:
    
      border-width: 1px 1px 1px 1px;
      margin: 10px 5px 10px 5px;
      padding: 0 0 10px 0;
    
    Related Story:
      http://www.pivotaltracker.com/story/show/41720871 (delivered)
      ("Teach scss-lint to detect unnecessary properties in shorthand")
    
    Change-Id: Ia130720d4abcb584ee23ceb955f6212f67f9aab9
    Reviewed-on: https://gerrit.causes.com/16944
    Reviewed-by: Alex Wallace <alex.wallace@causes.com>
    Tested-by: Joe Lencioni <joe.lencioni@causes.com>

---
+++ lib/scss_lint/linter/shorthand_linter.rb
@@ -0,0 +1,50 @@
+require 'sass'
+
+module SCSSLint
+  class Linter::ShorthandLinter < Linter
+    include LinterRegistry
+
+    class << self
+      def run(engine)
+        lints = []
+        engine.tree.each do |node|
+          if node.is_a?(Sass::Tree::PropNode)
+            lints << check_valid_shorthand_value(node)
+          end
+        end
+        lints.compact
+      end
+
+      def description
+        'Property values should use the shortest shorthand syntax allowed'
+      end
+
+    private
+
+      def check_valid_shorthand_value(prop_node)
+        if prop_node.value.is_a?(Sass::Script::String) &&
+           prop_node.value.to_s.strip =~ /\A(\S+\s+\S+\s+\S+(\s+\S+)?)\Z/
+          return create_lint(prop_node) unless valid_shorthand?($1)
+        end
+      end
+
+      def valid_shorthand?(shorthand)
+        values = shorthand.split(/\s+/)
+
+        if values[0] == values[1] &&
+           values[1] == values[2] &&
+           values[2] == values[3]
+          false
+        elsif values[0] == values[2] && values[1] == values[3]
+          false
+        elsif values[0] == values[2] && values[3].nil?
+          false
+        elsif values[1] == values[3]
+          false
+        else
+          true
+        end
+      end
+    end
+  end
+end
---
+++ spec/linter/shorthand_linter_spec.rb
@@ -0,0 +1,97 @@
+require 'spec_helper'
+
+describe SCSSLint::Linter::ShorthandLinter do
+  let(:engine) { SCSSLint::Engine.new(css) }
+  subject { SCSSLint::Linter::ShorthandLinter.run(engine) }
+
+  context 'when rule is empty' do
+    let(:css) { <<-EOS }
+      p {
+      }
+    EOS
+
+    it 'returns no lints' do
+      subject.should be_empty
+    end
+  end
+
+  context 'when rule contains properties with valid shorthand values' do
+    let(:css) { <<-EOS }
+      p {
+        border-radius: 1px 2px 1px 3px;
+        border-width: 1px;
+        color: rgba(0, 0, 0, .5);
+        margin: 1px 2px;
+        padding: 0 0 1px;
+      }
+    EOS
+
+    it 'returns no lints' do
+      subject.should be_empty
+    end
+  end
+
+  context 'when a property has a value repeated 4 times' do
+    let(:css) { <<-EOS }
+      p {
+        padding: 1px 1px 1px 1px;
+      }
+    EOS
+
+    it 'returns a lint' do
+      subject.count.should == 1
+    end
+
+    it 'reports the correct line for the lint' do
+      subject.first.line.should == 2
+    end
+  end
+
+  context 'when a property has its first two values repeated' do
+    let(:css) { <<-EOS }
+      p {
+        padding: 1px 2px 1px 2px;
+      }
+    EOS
+
+    it 'returns a lint' do
+      subject.count.should == 1
+    end
+
+    it 'reports correct lines for each lint' do
+      subject.first.line.should == 2
+    end
+  end
+
+  context 'when a property has its first value repeated in the third position' do
+    let(:css) { <<-EOS }
+      p {
+        padding: 1px 2px 1px;
+      }
+    EOS
+
+    it 'returns a lint' do
+      subject.count.should == 1
+    end
+
+    it 'reports correct lines for each lint' do
+      subject.first.line.should == 2
+    end
+  end
+
+  context 'when a property has its second value repeated in the fourth position' do
+    let(:css) { <<-EOS }
+      p {
+        padding: 1px 2px 3px 2px;
+      }
+    EOS
+
+    it 'returns a lint' do
+      subject.count.should == 1
+    end
+
+    it 'reports the correct line for the lint' do
+      subject.first.line.should == 2
+    end
+  end
+end
