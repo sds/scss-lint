b2451e44250161973b98d901a5fc5c6f1fb29bbc
Joe Lencioni + Emily Leathers <joe.lencioni+emily.leathers@causes.com>
2013-01-24 12:56:50 -0800

    Clarify ShorthandLinter spec structure
    - Group example groups by whether they pertain to full rules or to a
      single property.
    - Tidy example group names.
    
    Related Story:
      http://www.pivotaltracker.com/story/show/43148129
      ("Teach scss-lint about missed shorthand case")
    
    Change-Id: Iabb4d75cf9aaf8e8a85c66b1ccdd5a530afc2f68
    Reviewed-on: https://gerrit.causes.com/17748
    Reviewed-by: Alex Wallace <alex.wallace@causes.com>
    Tested-by: Joe Lencioni <joe.lencioni@causes.com>

--- spec/linter/shorthand_linter_spec.rb
+++ spec/linter/shorthand_linter_spec.rb
@@ -4,94 +4,98 @@ describe SCSSLint::Linter::ShorthandLinter do
   let(:engine) { SCSSLint::Engine.new(css) }
   subject { SCSSLint::Linter::ShorthandLinter.run(engine) }
 
-  context 'when rule is empty' do
-    let(:css) { <<-EOS }
-      p {
-      }
-    EOS
+  context 'when a rule' do
+    context 'is empty' do
+      let(:css) { <<-EOS }
+        p {
+        }
+      EOS
 
-    it 'returns no lints' do
-      subject.should be_empty
+      it 'returns no lints' do
+        subject.should be_empty
+      end
+    end
+
+    context 'contains properties with valid shorthand values' do
+      let(:css) { <<-EOS }
+        p {
+          border-radius: 1px 2px 1px 3px;
+          border-width: 1px;
+          color: rgba(0, 0, 0, .5);
+          margin: 1px 2px;
+          padding: 0 0 1px;
+        }
+      EOS
+
+      it 'returns no lints' do
+        subject.should be_empty
+      end
     end
   end
 
-  context 'when rule contains properties with valid shorthand values' do
-    let(:css) { <<-EOS }
-      p {
-        border-radius: 1px 2px 1px 3px;
-        border-width: 1px;
-        color: rgba(0, 0, 0, .5);
-        margin: 1px 2px;
-        padding: 0 0 1px;
-      }
-    EOS
+  context 'when a property' do
+    context 'has a value repeated 4 times' do
+      let(:css) { <<-EOS }
+        p {
+          padding: 1px 1px 1px 1px;
+        }
+      EOS
 
-    it 'returns no lints' do
-      subject.should be_empty
-    end
-  end
+      it 'returns a lint' do
+        subject.count.should == 1
+      end
 
-  context 'when a property has a value repeated 4 times' do
-    let(:css) { <<-EOS }
-      p {
-        padding: 1px 1px 1px 1px;
-      }
-    EOS
-
-    it 'returns a lint' do
-      subject.count.should == 1
+      it 'reports the correct line for the lint' do
+        subject.first.line.should == 2
+      end
     end
 
-    it 'reports the correct line for the lint' do
-      subject.first.line.should == 2
-    end
-  end
+    context 'has its first two values repeated' do
+      let(:css) { <<-EOS }
+        p {
+          padding: 1px 2px 1px 2px;
+        }
+      EOS
 
-  context 'when a property has its first two values repeated' do
-    let(:css) { <<-EOS }
-      p {
-        padding: 1px 2px 1px 2px;
-      }
-    EOS
+      it 'returns a lint' do
+        subject.count.should == 1
+      end
 
-    it 'returns a lint' do
-      subject.count.should == 1
-    end
-
-    it 'reports correct lines for each lint' do
-      subject.first.line.should == 2
-    end
-  end
-
-  context 'when a property has its first value repeated in the third position' do
-    let(:css) { <<-EOS }
-      p {
-        padding: 1px 2px 1px;
-      }
-    EOS
-
-    it 'returns a lint' do
-      subject.count.should == 1
+      it 'reports correct lines for each lint' do
+        subject.first.line.should == 2
+      end
     end
 
-    it 'reports correct lines for each lint' do
-      subject.first.line.should == 2
+    context 'has its first value repeated in the third position' do
+      let(:css) { <<-EOS }
+        p {
+          padding: 1px 2px 1px;
+        }
+      EOS
+
+      it 'returns a lint' do
+        subject.count.should == 1
+      end
+
+      it 'reports correct lines for each lint' do
+        subject.first.line.should == 2
+      end
     end
-  end
 
-  context 'when a property has its second value repeated in the fourth position' do
-    let(:css) { <<-EOS }
-      p {
-        padding: 1px 2px 3px 2px;
-      }
-    EOS
+    context 'has its second value repeated in the fourth position' do
+      let(:css) { <<-EOS }
+        p {
+          padding: 1px 2px 3px 2px;
+        }
+      EOS
 
-    it 'returns a lint' do
-      subject.count.should == 1
-    end
+      it 'returns a lint' do
+        subject.count.should == 1
+      end
 
-    it 'reports the correct line for the lint' do
-      subject.first.line.should == 2
+      it 'reports the correct line for the lint' do
+        subject.first.line.should == 2
+      end
     end
   end
 end
