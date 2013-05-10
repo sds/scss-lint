38094fe085fac7ca085a6c3e14c091545a45f347
Shane da Silva <shane@causes.com>
2013-05-10 15:51:33 -0700

    Add support for interpolation to SingleLinePerSelectorLinter
    Previously, the `SingleLinePerSelectorLinter` would choke on
    interpolated strings. This has been fixed by implementing a more
    comprehensive check that properly deals with Sass::Script::Nodes in a
    rule's comma sequence.
    
    In the process of doing so, support was also added for not reporting a
    lint when a selector in a multi-line rule had a comment on the end of
    it, e.g.
    
        .foo, // A comment
        .bar {
        }
    
    This invalid reporting was fixed by normalizing the whitespace appearing
    around newline characters in the rule, since there were extra whitespace
    characters being left behind when the Sass parser automatically removed
    the comments from the parse tree. This fixes #10.
    
    Change-Id: I300ce8a8fc5672c86585f375ff861b9ed50bc1f3
    Reviewed-on: https://gerrit.causes.com/22121
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Shane da Silva <shane@causes.com>

--- lib/scss_lint/linter/single_line_per_selector_linter.rb
+++ lib/scss_lint/linter/single_line_per_selector_linter.rb
@@ -5,12 +5,36 @@ module SCSSLint
     include LinterRegistry
 
     def visit_rule(node)
-      add_lint(node) unless node.rule.grep(/,[^\n]/).empty?
+      add_lint(node) if invalid_comma_placement? node
       yield # Continue linting children
     end
 
     def description
       'Each selector should be on its own line'
+    end
+
+  private
+
+    # A comma is invalid if it starts the line or is not the end of the line
+    def invalid_comma_placement?(node)
+      normalize_spacing(condense_to_string(node.rule)) =~ /\n,|,[^\n]/
+    end
+
+    # Since RuleNode.rule returns an array containing both String and
+    # Sass::Script::Nodes, we need to condense it into a single string that we
+    # can run a regex against.
+    def condense_to_string(sequence_list)
+      sequence_list.inject('') do |combined, string_or_script|
+        combined + (string_or_script.is_a?(String) ? string_or_script
+                                                   : string_or_script.to_sass)
+      end
+    end
+
+    # Removes extra spacing between lines in a comma-separated sequence due to
+    # comments being removed in the parse phase. This makes it easy to check if
+    # a comma is where belongs.
+    def normalize_spacing(string_sequence)
+      string_sequence.gsub(/,[^\S\n]*\n\s*/, ",\n")
     end
   end
 end
--- spec/linter/single_line_per_selector_linter_spec.rb
+++ spec/linter/single_line_per_selector_linter_spec.rb
@@ -50,6 +50,31 @@ describe SCSSLint::Linter::SingleLinePerSelector do
     end
   end
 
+  context 'when commas are not at the end of the line' do
+    let(:css) { <<-EOS }
+      .foo
+      , .bar {
+      }
+    EOS
+
+    it 'returns a lint' do
+      subject.count.should == 1
+    end
+  end
+
+  context 'when commas are on their own line' do
+    let(:css) { <<-EOS }
+      .foo
+      ,
+      .bar {
+      }
+    EOS
+
+    it 'returns a lint' do
+      subject.count.should == 1
+    end
+  end
+
   context 'when nested rule contains multiple selectors on the same line' do
     let(:css) { <<-EOS }
       #foo {
@@ -67,6 +92,49 @@ describe SCSSLint::Linter::SingleLinePerSelector do
 
     it 'reports the last line of the multi-selector declaration for the lint' do
       subject.first.line.should == 5
+    end
+  end
+
+  context 'when rule contains interpolated selectors' do
+    let(:css) { <<-EOS }
+      .first,
+      \#{interpolated-selector}.thing,
+      .third {
+      }
+    EOS
+
+    it 'returns no lints' do
+      subject.count.should == 0
+    end
+  end
+
+  context 'when rule contains an interpolated selector not on its own line' do
+    let(:css) { <<-EOS }
+      .first,
+      .second, \#{interpolated-selector}.thing,
+      .fourth {
+      }
+    EOS
+
+    it 'returns a lint' do
+      subject.count.should == 1
+    end
+
+    it 'reports the last line of the multi-selector declaration for the lint' do
+      subject.first.line.should == 3
+    end
+  end
+
+  context 'when rule contains an inline comment' do
+    let(:css) { <<-EOS }
+      .first,  /* A comment */
+      .second, // Another comment
+      .third {
+      }
+    EOS
+
+    it 'returns no lints' do
+      subject.count.should == 0
     end
   end
 end
