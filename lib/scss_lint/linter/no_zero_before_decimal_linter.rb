ca9c761f15b93a79b107ca2b5486b81e9e60f862
Shane da Silva <shane@causes.com>
2013-08-30 21:16:49 -0700

    Add NoZeroBeforeDecimalLinter
    Add a linter that checks for unnecessary leading zeros in numbers with a
    fractional component. In other words, prefer `.5` over `0.5`.
    
    This is unfortunately not a complete implementation, as numbers inside
    Sass Script will not be detected in this way due to the way the Sass
    parser replaces the original string with the resultant
    `Sass::Script::Literal`. For example, '.5' will become the Float `0.5`,
    so we can't detect after the fact whether the value was written with an
    unnecessary leading zero. Concretely, values inside function or mixin
    arguments will not be linted.
    
    Luckily, this implementation handles a significant number of cases.
    
    Perhaps one day we will contribute a commit to the upstream `sass` gem
    to be able to handle the general case.
    
    Change-Id: I069a4bd3095ddd02a5c31d1f5dfbe5c1b7ce77fa
    Reviewed-on: https://gerrit.causes.com/27812
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Shane da Silva <shane@causes.com>

---
+++ lib/scss_lint/linter/no_zero_before_decimal_linter.rb
@@ -0,0 +1,22 @@
+module SCSSLint
+  class Linter::NoZeroBeforeDecimalLinter < Linter
+    include LinterRegistry
+
+    def visit_prop(node)
+      # Misleading, but anything that isn't Sass Script is considered an
+      # `identifier` in the context of a Sass::Script::String.
+      return unless node.value.is_a?(Sass::Script::String) && node.value.type == :identifier
+
+      # Remove string chunks (e.g. `"hello" 3 'world'` -> `3`
+      non_string_values = node.value.value.gsub(/"[^"]*"|'[^']'/, '').split
+
+      non_string_values.each do |value|
+        add_lint(node) if value =~ /\b0\.\d+/
+      end
+    end
+
+    def description
+      'Leading zero should be omitted in fractional values'
+    end
+  end
+end
