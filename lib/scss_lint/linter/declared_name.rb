85adedff6212a62892bf1020d812b53d1c809888
Shane da Silva <shane@causes.com>
2013-09-18 23:46:09 -0700

    Make DeclaredName lints more specific
    Change the descriptions of lints reported by `DeclaredName` to be
    relevant to the particular context (e.g. whether the incorrectly named
    thing was a function, mixin, or variable).
    
    Change-Id: I2191c1a3bb4f512b7d28e377eaedce4c0ec0a849
    Reviewed-on: https://gerrit.causes.com/28832
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Greg Hurrell <greg@causes.com>

--- lib/scss_lint/linter/declared_name.rb
+++ lib/scss_lint/linter/declared_name.rb
@@ -3,29 +3,29 @@ module SCSSLint
     include LinterRegistry
 
     def visit_function(node)
-      check(node)
+      check(node, 'function')
       yield # Continue into content block of this function definition
     end
 
     def visit_mixindef(node)
-      check(node)
+      check(node, 'mixin')
       yield # Continue into content block of this mixin definition
     end
 
     def visit_variable(node)
-      check(node)
+      check(node, 'variable')
       yield # Continue into expression tree for this variable definition
-    end
-
-    def description
-      'Names of variables, functions, mixins, and placeholders should be ' <<
-      'lowercase and use hyphens instead of underscores.'
     end
 
   private
 
-    def check(node)
-      add_lint(node) if node_has_bad_name?(node)
+    def check(node, node_type)
+      if node_has_bad_name?(node)
+        fixed_name = node.name.downcase.gsub(/_/, '-')
+
+        add_lint(node, "Name of #{node_type} `#{node.name}` should " <<
+                       "be written in lowercase as `#{fixed_name}`")
+      end
     end
   end
 end
