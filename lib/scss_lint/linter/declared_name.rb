2abf248042d8499879f951af8364c17a3ecf9968
Shane da Silva <shane@causes.com>
2013-09-18 23:35:48 -0700

    Remove placeholder check from DeclaredName linter
    The `CapitalizationInSelector` linter now checks for improper
    capitalization for placeholder selectors. Thus we can remove the
    placeholder-related checks from the `DeclardName` linter (or else we'll
    get double-reporting on the error).
    
    Change-Id: If964bcc8cbc1b98f192c04d35c435c9ebe64c8be
    Reviewed-on: https://gerrit.causes.com/28831
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Greg Hurrell <greg@causes.com>

--- lib/scss_lint/linter/declared_name.rb
+++ lib/scss_lint/linter/declared_name.rb
@@ -12,11 +12,6 @@ module SCSSLint
       yield # Continue into content block of this mixin definition
     end
 
-    def visit_rule(node)
-      add_lint(node) if selector_has_bad_placeholder?(node.rule)
-      yield # Continue linting into content block of this rule definition
-    end
-
     def visit_variable(node)
       check(node)
       yield # Continue into expression tree for this variable definition
