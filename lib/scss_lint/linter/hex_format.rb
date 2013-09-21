4b746e6d4c7dd2584421f9535d88845cc36457ac
Shane da Silva <shane@causes.com>
2013-09-20 23:24:58 -0700

    Simplify HexFormat linter string searching
    The `HexFormat` linter was using `visit_prop` with a manual check to see
    if the value of the property was a `Sass::Script::String`, rather than
    just using `visit_script_string`.
    
    Switching to `visit_script_string` simplifies the code.
    
    Change-Id: I84a64b97071b74d9d37ff40360f4683a2e557368
    Reviewed-on: https://gerrit.causes.com/29015
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Shane da Silva <shane@causes.com>

--- lib/scss_lint/linter/hex_format.rb
+++ lib/scss_lint/linter/hex_format.rb
@@ -2,23 +2,19 @@ module SCSSLint
   class Linter::HexFormat < Linter
     include LinterRegistry
 
-    def visit_prop(node)
-      if node.value.is_a?(Sass::Script::String) &&
-         node.value.type == :identifier
-
-        node.value.value.scan(HEX_REGEX) do |match|
-          add_hex_lint(node, match.first) unless valid_hex_format?(match.first)
-        end
-      end
-
-      yield # Continue visiting children
-    end
-
     def visit_script_color(node)
       return unless node.original && node.original.match(HEX_REGEX)
 
       unless valid_hex_format?(node.original[HEX_REGEX, 1])
         add_hex_lint(node, node.original)
+      end
+    end
+
+    def visit_script_string(node)
+      return unless node.type == :identifier
+
+      node.value.scan(HEX_REGEX) do |match|
+        add_hex_lint(node, match.first) unless valid_hex_format?(match.first)
       end
     end
 
