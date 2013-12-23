fdc37aefe20943d49eeb547e0e67f03f27fe46ea
Mike Sherov <mike.sherov@gmail.com>
2013-12-23 15:38:34 -0500

    ID with Extraneous Selector Linter no longer falsely reports IDs with only pseudo selectors.

--- lib/scss_lint/linter/id_with_extraneous_selector.rb
+++ lib/scss_lint/linter/id_with_extraneous_selector.rb
@@ -7,7 +7,7 @@ module SCSSLint
       id_sel = seq.members.find { |simple| simple.is_a?(Sass::Selector::Id) }
       return unless id_sel
 
-      if seq.members.any? { |simple| !simple.is_a?(Sass::Selector::Id) }
+      if seq.members.any? { |simple| !simple.is_a?(Sass::Selector::Id) && !simple.is_a?(Sass::Selector::Pseudo) }
         add_lint(seq, "Selector `#{seq}` can be simplified to `#{id_sel}`, " <<
                       'since IDs should be uniquely identifying')
       end
