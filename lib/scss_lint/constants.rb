7bc4cff397ec0d6b21b4b373c310d37e91f3198d
Shane da Silva <shane@causes.com>
2014-01-04 15:18:36 -0800

    Move known CSS properties into data file
    Including the list of properties in the class definition was a bit
    unconventional. Move the property list into a separate file.
    
    At the same time, update the list of properties using the data from
    http://www.chromestatus.com/metrics/css/popularity (which was suggested
    in #29).
    
    This brings the property count from 256 up to 465 unique properties.
    
    Closes #61, #65.
    
    Change-Id: I916bf8db79c6f3c9eb7b2575c6b0f28caac4d5e2
    Reviewed-on: https://gerrit.causes.com/32422
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Shane da Silva <shane@causes.com>

--- lib/scss_lint/constants.rb
+++ lib/scss_lint/constants.rb
@@ -1,6 +1,7 @@
 # Global application constants.
 module SCSSLint
   SCSS_LINT_HOME = File.realpath(File.join(File.dirname(__FILE__), '..', '..'))
+  SCSS_LINT_DATA = File.join(SCSS_LINT_HOME, 'data')
 
   REPO_URL = 'https://github.com/causes/scss-lint'
   BUG_REPORT_URL = "#{REPO_URL}/issues"
