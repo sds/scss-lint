b5d51e17d1d48cc315e3e81ec10a56078571df13
Shane da Silva <shane@dasilva.io>
2014-03-29 16:43:22 -0700

    Remove description method from EmptyRule
    The `description` method is being deprecated.
    
    Change-Id: I632b48097c628b6140b330745dfef15b059174ff
    Reviewed-on: http://gerrit.causes.com/36722
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Shane da Silva <shane@causes.com>

--- lib/scss_lint/linter/empty_rule.rb
+++ lib/scss_lint/linter/empty_rule.rb
@@ -4,12 +4,8 @@ module SCSSLint
     include LinterRegistry
 
     def visit_rule(node)
-      add_lint(node) if node.children.empty?
+      add_lint(node, 'Empty rule') if node.children.empty?
       yield # Continue linting children
-    end
-
-    def description
-      'Empty rule'
     end
   end
 end
