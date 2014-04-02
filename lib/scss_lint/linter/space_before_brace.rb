4d7ecb0532288f16c29216d4d76b5babf63394f4
Shane da Silva <shane@causes.com>
2014-04-01 22:13:03 -0700

    Reword SpaceBeforeBrace lint description
    Alter slightly to refer to the specific curly brace rather than curly
    braces in general, and use backticks instead of parentheses to make it
    easier to read.
    
    Change-Id: Ia068dae02f5dc6ffae8dea60928bc65ed299f4a0
    Reviewed-on: http://gerrit.causes.com/36829
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Shane da Silva <shane@causes.com>

--- lib/scss_lint/linter/space_before_brace.rb
+++ lib/scss_lint/linter/space_before_brace.rb
@@ -8,11 +8,11 @@ module SCSSLint
 
         if config['allow_extra_spaces'] && node_on_single_line(node)
           line.scan /[^"# ]\{/ do |match|
-            add_lint(index + 1, 'Opening curly braces ({) should be preceded by at least one space')
+            add_lint(index + 1, 'Opening curly brace `{` should be preceded by at least one space')
           end
         else
           line.scan /[^"#](?<![^ ] )\{/ do |match|
-            add_lint(index + 1, 'Opening curly braces ({) should be preceded by one space')
+            add_lint(index + 1, 'Opening curly brace `{` should be preceded by one space')
           end
         end
       end
