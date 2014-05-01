58e28859f00d43bf4104bef270256c442d817ef1
Nils Gemeinhardt <git@nilsgemeinhardt.de>
2014-05-01 19:07:23 +0200

    Fix bug in `ZeroUnit` when it is not a dimension
    only for dimensions the unit identifier is optional

--- lib/scss_lint/linter/zero_unit.rb
+++ lib/scss_lint/linter/zero_unit.rb
@@ -7,19 +7,21 @@ module SCSSLint
       return unless node.type == :identifier
 
       node.value.scan(ZERO_UNIT_REGEX) do |match|
+        next unless zero_with_length?(match.first)
         add_lint(node, MESSAGE_FORMAT % match.first)
       end
     end
 
     def visit_script_number(node)
       length = source_from_range(node.source_range)[ZERO_UNIT_REGEX, 1]
+      return unless zero_with_length?(length)
 
-      if node.value == 0 && zero_with_units?(length)
-        add_lint(node, MESSAGE_FORMAT % length)
-      end
+      add_lint(node, MESSAGE_FORMAT % length)
     end
 
   private
+
+    UNITS = %w[em ex ch rem vw vh vmin vmax cm mm in pt pc px]
 
     ZERO_UNIT_REGEX = /
       \b
@@ -30,8 +32,8 @@ module SCSSLint
 
     MESSAGE_FORMAT = '`%s` should be written without units as `0`'
 
-    def zero_with_units?(string)
-      string =~ /^0[a-z]+/
+    def zero_with_length?(string)
+      string =~ /^0([a-z]+)/ && UNITS.include?(Regexp.last_match(1))
     end
   end
 end
