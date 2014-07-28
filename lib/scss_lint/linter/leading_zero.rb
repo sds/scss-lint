ad752fd74581bf3a407b6a9d4b309ef191f3bdfc
Shane da Silva <shane.dasilva@brigade.com>
2014-07-28 15:41:22 -0700

    Fix crash in LeadingZero
    Due to an upstream Sass bug (https://github.com/sass/sass/issues/1343),
    check for the case where a source range is incorrect and pass if it is.
    
    This means we may not report some lints, but this case should be
    relatively rare it's better than crashing.
    
    Change-Id: I079e6a6681400ed574a9dda035c9555748188ee1
    Reviewed-on: http://gerrit.causes.com/41017
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

--- lib/scss_lint/linter/leading_zero.rb
+++ lib/scss_lint/linter/leading_zero.rb
@@ -14,7 +14,10 @@ module SCSSLint
     end
 
     def visit_script_number(node)
-      return unless number = source_from_range(node.source_range)[FRACTIONAL_DIGIT_REGEX, 1]
+      # TODO: Remove once https://github.com/sass/sass/issues/1343 is fixed
+      return unless source = source_from_range(node.source_range)
+
+      return unless number = source[FRACTIONAL_DIGIT_REGEX, 1]
 
       check_number(node, number)
     end
