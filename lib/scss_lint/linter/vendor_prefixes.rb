efba9af3db72762ed4c071aaaf88a780080bff8b
David Clark <david.dave.clark@gmail.com>
2014-10-07 20:53:57 -0700

    Improving VendorPrefixes linter

--- lib/scss_lint/linter/vendor_prefixes.rb
+++ lib/scss_lint/linter/vendor_prefixes.rb
@@ -2,17 +2,62 @@ module SCSSLint
   class Linter::VendorPrefixes < Linter
     include LinterRegistry
 
+    def visit_root(_node)
+      @identifiers = Set.new extract_identifiers_from_config
+      @identifiers.merge(Set.new config['include'])
+      @exclusions = Set.new config['exclude']
+      yield
+    end
+
     def check_node(node)
       name = node.name.is_a?(Array) ? node.name.join : node.name
-      # Remove '@' from @keyframes node name
-      name = name.gsub('@', '')
-      return unless name.start_with?('-', '_')
-      add_lint(node, 'Vendor prefixes should not be used.')
+      # Ignore '@' from @keyframes node name
+      check_identifier(node, name.gsub(/^@/, ''))
+
+      # Check for values
+      if node.respond_to?('value') && node.value.respond_to?('to_sass')
+        check_identifier(node, node.value.to_sass)
+      end
     end
 
     alias_method :visit_prop, :check_node
     alias_method :visit_pseudo, :check_node
     alias_method :visit_directive, :check_node
+
+  private
+
+    def check_identifier(node, identifier)
+      return unless identifier =~ /^[_-]/
+      # Strip vendor prefix to check against identifiers.
+      # (Also strip closing parentheticals from values like linear-gradient.)
+      strippedIdentifier = identifier.gsub(/(^[_-][a-zA-Z0-9_]+-|\(.*\))/, '')
+      return if @exclusions.include?(strippedIdentifier)
+      return unless @identifiers.include?(strippedIdentifier)
+      add_lint(node, 'Avoid vendor prefixes.')
+    end
+
+    def extract_identifiers_from_config
+      case config['identifier_list']
+      when nil
+        nil # No custom order specified
+      when Array
+        config['identifier_list']
+      when String
+        begin
+          file = File.open(File.join(SCSS_LINT_DATA,
+                                     'prefixed-identifiers',
+                                     "#{config['identifier_list']}.txt"))
+          file.read.split("\n").reject { |line| line =~ /^(#|\s*$)/ }
+        rescue Errno::ENOENT
+          raise SCSSLint::LinterError,
+                "Identifier list '#{config['identifier_list']}' does not exist"
+        end
+      else
+        raise SCSSLint::LinterError,
+              'Invalid identifier list specified -- must be the name of a '\
+              'preset or an array of strings'
+      end
+    end
 
   end
 end
