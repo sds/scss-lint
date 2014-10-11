6b82fc68447d2963a8a995ad1a750550f7519e6a
David Clark <david.dave.clark@gmail.com>
2014-10-11 15:08:03 -0700

    Modified SpaceBeforeBrace linter to accept new_line style option

--- lib/scss_lint/linter/space_before_brace.rb
+++ lib/scss_lint/linter/space_before_brace.rb
@@ -27,19 +27,46 @@ module SCSSLint
 
     def check_for_space(node, string)
       line = node.source_range.end_pos.line
-      char_before_is_whitespace = ["\n", ' '].include?(string[-2])
 
       if config['allow_single_line_padding'] && node_on_single_line?(node)
-        unless char_before_is_whitespace
-          add_lint(line, 'Opening curly brace `{` should be ' \
-                         'preceded by at least one space')
-        end
+        return unless string[-2] != ' '
+        add_lint(line, 'Opening curly brace in a single line rule set '\
+                       '`{` should be preceded by at least one space')
       else
-        if !char_before_is_whitespace || string[-3] == ' '
-          add_lint(line, 'Opening curly brace `{` should be ' \
-                         'preceded by one space')
-        end
+        return unless chars_before_incorrect(string)
+        style_message = (config['style'] == 'new_line') ? 'a new line' : 'one space'
+        add_lint(line, 'Opening curly brace `{` should be ' \
+                       "preceded by #{style_message}")
       end
+    end
+
+    # Check if the characters before the end of the string
+    # are not what they should be
+    def chars_before_incorrect(string)
+      if config['style'] != 'new_line'
+        return !single_space_before(string)
+      end
+      !newline_before_nonwhitespace(string)
+    end
+
+    # Check if there is one space and only one
+    # space before the end of the string
+    def single_space_before(string)
+      return false if string[-2] != ' '
+      return false if string[-3] == ' '
+      true
+    end
+
+    # Check if, starting from the end of a string
+    # and moving backwards, towards the beginning,
+    # we find a new line before any non-whitespace characters
+    def newline_before_nonwhitespace(string)
+      offset = -2
+      while /\S/.match(string[offset]).nil?
+        return true if string[offset] == "\n"
+        offset -= 1
+      end
+      false
     end
   end
 end
