d9f5af12a29838125b8fc0ae677b3850291445c7
David Clark <david.dave.clark@gmail.com>
2014-10-16 19:55:23 -0700

    Add mixins and nested rule checking to DeclarationOrder
    Change-Id: I3f13160add6513c0f7cd785032d54a4689cb680a
    Reviewed-on: http://gerrit.causes.com/43809
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

--- lib/scss_lint/linter/declaration_order.rb
+++ lib/scss_lint/linter/declaration_order.rb
@@ -3,35 +3,54 @@ module SCSSLint
   class Linter::DeclarationOrder < Linter
     include LinterRegistry
 
-    DECLARATION_ORDER = [
-      Sass::Tree::ExtendNode,
-      Sass::Tree::PropNode,
-      Sass::Tree::RuleNode,
-    ]
-
     def visit_rule(node)
-      children = node.children.select { |n| important_node?(n) }
-                              .map(&:class)
-
-      sorted_children = children.sort do |a, b|
-        DECLARATION_ORDER.index(a) <=> DECLARATION_ORDER.index(b)
-      end
-
-      if children != sorted_children
-        add_lint(node.children.first, MESSAGE)
-      end
-
+      check_node(node)
       yield # Continue linting children
     end
 
   private
 
     MESSAGE =
-      'Rule sets should start with @extend declarations, followed by ' \
-      'properties and nested rule sets, in that order'
+      'Rule sets should be ordered as follows: '\
+      '@extends, @includes without @content, ' \
+      'properties, @includes with @content, ' \
+      'nested rule sets'
+
+    MIXIN_WITH_CONTENT = 'mixin_with_content'
+
+    DECLARATION_ORDER = [
+      Sass::Tree::ExtendNode,
+      Sass::Tree::MixinNode,
+      Sass::Tree::PropNode,
+      MIXIN_WITH_CONTENT,
+      Sass::Tree::RuleNode,
+    ]
 
     def important_node?(node)
       DECLARATION_ORDER.include? node.class
+    end
+
+    def check_node(node)
+      children = node.children.select { |n| important_node?(n) }
+                              .map { |n| check_for_children(n) }
+
+      sorted_children = children.sort do |a, b|
+        DECLARATION_ORDER.index(a) <=> DECLARATION_ORDER.index(b)
+      end
+
+      return unless children != sorted_children
+      add_lint(node.children.first, MESSAGE)
+    end
+
+    def check_for_children(node)
+      # If the node has no children, return the class.
+      return node.class unless node.has_children
+      # Check the node's children just as the node is checked.
+      check_node(node)
+      # If the node is a mixin with children, indicate that;
+      # otherwise, just return the class.
+      return node.class unless node.is_a?(Sass::Tree::MixinNode)
+      MIXIN_WITH_CONTENT
     end
   end
 end
--- spec/scss_lint/linter/declaration_order_spec.rb
+++ spec/scss_lint/linter/declaration_order_spec.rb
@@ -91,4 +91,476 @@ describe SCSSLint::Linter::DeclarationOrder do
 
     it { should report_lint }
   end
+
+  context 'when nested rule set' do
+    context 'contains @extend before a property' do
+      let(:css) { <<-CSS }
+        p {
+          a {
+            @extend foo;
+            color: #f00;
+          }
+        }
+      CSS
+
+      it { should_not report_lint }
+    end
+
+    context 'contains @extend after a property' do
+      let(:css) { <<-CSS }
+        p {
+          a {
+            color: #f00;
+            @extend foo;
+          }
+        }
+      CSS
+
+      it { should report_lint }
+    end
+
+    context 'contains @extend after nested rule set' do
+      let(:css) { <<-CSS }
+        p {
+          a {
+            span {
+              color: #000;
+            }
+            @extend foo;
+          }
+        }
+      CSS
+
+      it { should report_lint }
+    end
+  end
+
+  context 'when @include appears' do
+    context 'before a property and rule set' do
+      let(:css) { <<-CSS }
+        .error {
+          @include warn;
+          color: #f00;
+          a {
+            color: #ccc;
+          }
+        }
+      CSS
+
+      it { should_not report_lint }
+    end
+
+    context 'after a property and before a rule set' do
+      let(:css) { <<-CSS }
+        .error {
+          color: #f00;
+          @include warn;
+          a {
+            color: #ccc;
+          }
+        }
+      CSS
+
+      it { should report_lint }
+    end
+  end
+
+  context 'when @include that features @content appears' do
+    context 'before a property' do
+      let(:css) { <<-CSS }
+        .foo {
+          @include breakpoint("phone") {
+            color: #ccc;
+          }
+          color: #f00;
+        }
+      CSS
+
+      it { should report_lint }
+    end
+
+    context 'after a property' do
+      let(:css) { <<-CSS }
+        .foo {
+          color: #f00;
+          @include breakpoint("phone") {
+            color: #ccc;
+          }
+        }
+      CSS
+
+      it { should_not report_lint }
+    end
+
+    context 'before an @extend' do
+      let(:css) { <<-CSS }
+        .foo {
+          @include breakpoint("phone") {
+            color: #ccc;
+          }
+          @extend .bar;
+        }
+      CSS
+
+      it { should report_lint }
+    end
+
+    context 'before a rule set' do
+      let(:css) { <<-CSS }
+        .foo {
+          @include breakpoint("phone") {
+            color: #ccc;
+          }
+          a {
+            color: #fff;
+          }
+        }
+      CSS
+
+      it { should_not report_lint }
+    end
+
+    context 'after a rule set' do
+      let(:css) { <<-CSS }
+        .foo {
+          a {
+            color: #fff;
+          }
+          @include breakpoint("phone") {
+            color: #ccc;
+          }
+        }
+      CSS
+
+      it { should report_lint }
+    end
+
+    context 'with its own nested rule set' do
+      context 'before a property' do
+        let(:css) { <<-CSS }
+          .foo {
+            background: #fff;
+            @include breakpoint("phone") {
+              a {
+                color: #000;
+              }
+              color: #ccc;
+            }
+          }
+        CSS
+
+        it { should report_lint }
+      end
+
+      context 'after a property' do
+        let(:css) { <<-CSS }
+          .foo {
+            background: #fff;
+            @include breakpoint("phone") {
+              color: #ccc;
+              a {
+                color: #000;
+              }
+            }
+          }
+        CSS
+
+        it { should_not report_lint }
+      end
+    end
+  end
+
+  context 'when the nesting is crazy deep' do
+    context 'and nothing is wrong' do
+      let(:css) { <<-CSS }
+        div {
+          ul {
+            @extend .thing;
+            li {
+              @include box-shadow(yes);
+              background: green;
+              a {
+                span {
+                  @include border-radius(5px);
+                  color: #000;
+                }
+              }
+            }
+          }
+        }
+      CSS
+
+      it { should_not report_lint }
+    end
+
+    context 'and something is wrong' do
+      let(:css) { <<-CSS }
+        div {
+          ul {
+            li {
+              a {
+                span {
+                  color: #000;
+                  @include border-radius(5px);
+                }
+              }
+            }
+          }
+        }
+      CSS
+
+      it { should report_lint }
+    end
+  end
+
+  context 'when inside a @media query and rule set' do
+    context 'contains @extend before a property' do
+      let(:css) { <<-CSS }
+        @media only screen and (max-width: 1px) {
+          a {
+            @extend foo;
+            color: #f00;
+          }
+        }
+      CSS
+
+      it { should_not report_lint }
+    end
+
+    context 'contains @extend after a property' do
+      let(:css) { <<-CSS }
+        @media only screen and (max-width: 1px) {
+          a {
+            color: #f00;
+            @extend foo;
+          }
+        }
+      CSS
+
+      it { should report_lint }
+    end
+
+    context 'contains @extend after nested rule set' do
+      let(:css) { <<-CSS }
+        @media only screen and (max-width: 1px) {
+          a {
+            span {
+              color: #000;
+            }
+            @extend foo;
+          }
+        }
+      CSS
+
+      it { should report_lint }
+    end
+  end
+
+  context 'when a pseudo-element appears before a property' do
+    let(:css) { <<-CSS }
+      a {
+        &:hover {
+          color: #000;
+        }
+        color: #fff;
+      }
+    CSS
+
+    it { should report_lint }
+  end
+
+  context 'when a pseudo-element appears after a property' do
+    let(:css) { <<-CSS }
+      a {
+        color: #fff;
+        &:focus {
+          color: #000;
+        }
+      }
+    CSS
+
+    it { should_not report_lint }
+  end
+
+  context 'when a chained selector appears after a property' do
+    let(:css) { <<-CSS }
+      a {
+        color: #fff;
+        &.is-active {
+          color: #000;
+        }
+      }
+    CSS
+
+    it { should_not report_lint }
+  end
+
+  context 'when a chained selector appears before a property' do
+    let(:css) { <<-CSS }
+      a {
+        &.is-active {
+          color: #000;
+        }
+        color: #fff;
+      }
+    CSS
+
+    it { should report_lint }
+  end
+
+  context 'when a selector with parent reference appears after a property' do
+    let(:css) { <<-CSS }
+      a {
+        color: #fff;
+        .is-active & {
+          color: #000;
+        }
+      }
+    CSS
+
+    it { should_not report_lint }
+  end
+
+  context 'when a selector with parent reference appears before a property' do
+    let(:css) { <<-CSS }
+      a {
+        .is-active & {
+          color: #000;
+        }
+        color: #fff;
+      }
+    CSS
+
+    it { should report_lint }
+  end
+
+  context 'when a pseudo-element appears after a property' do
+    let(:css) { <<-CSS }
+      a {
+        color: #fff;
+        &:before {
+          color: #000;
+        }
+      }
+    CSS
+
+    it { should_not report_lint }
+  end
+
+  context 'when a pseudo-element appears before a property' do
+    let(:css) { <<-CSS }
+      a {
+        &:before {
+          color: #000;
+        }
+        color: #fff;
+      }
+    CSS
+
+    it { should report_lint }
+  end
+
+  context 'when a direct descendent appears after a property' do
+    let(:css) { <<-CSS }
+      a {
+        color: #fff;
+        > .foo {
+          color: #000;
+        }
+      }
+    CSS
+
+    it { should_not report_lint }
+  end
+
+  context 'when a direct descendent appears before a property' do
+    let(:css) { <<-CSS }
+      a {
+        > .foo {
+          color: #000;
+        }
+        color: #fff;
+      }
+    CSS
+
+    it { should report_lint }
+  end
+
+  context 'when an adjacent sibling appears after a property' do
+    let(:css) { <<-CSS }
+      a {
+        color: #fff;
+        & + .foo {
+          color: #000;
+        }
+      }
+    CSS
+
+    it { should_not report_lint }
+  end
+
+  context 'when an adjacent sibling appears before a property' do
+    let(:css) { <<-CSS }
+      a {
+        & + .foo {
+          color: #000;
+        }
+        color: #fff;
+      }
+    CSS
+
+    it { should report_lint }
+  end
+
+  context 'when a general sibling appears after a property' do
+    let(:css) { <<-CSS }
+      a {
+        color: #fff;
+        & ~ .foo {
+          color: #000;
+        }
+      }
+    CSS
+
+    it { should_not report_lint }
+  end
+
+  context 'when a general sibling appears before a property' do
+    let(:css) { <<-CSS }
+      a {
+        & ~ .foo {
+          color: #000;
+        }
+        color: #fff;
+      }
+    CSS
+
+    it { should report_lint }
+  end
+
+  context 'when a descendent appears after a property' do
+    let(:css) { <<-CSS }
+      a {
+        color: #fff;
+        .foo {
+          color: #000;
+        }
+      }
+    CSS
+
+    it { should_not report_lint }
+  end
+
+  context 'when a descendent appears before a property' do
+    let(:css) { <<-CSS }
+      a {
+        .foo {
+          color: #000;
+        }
+        color: #fff;
+      }
+    CSS
+
+    it { should report_lint }
+  end
 end
