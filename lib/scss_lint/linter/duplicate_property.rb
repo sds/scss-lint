sds/scss-lint

commit 4fd093c38b595a5f104be343218551f88bada5ff

Fix DuplicateProperty handling of nested rule sets
The linter would not report duplicate properties in nested rule sets.

Fix #295 by `yield`ing to continue visiting children.

While here, also did a bit of cleanup.

Change-Id: I73ad40e3cf65d72b0792e77a62e33fb75cc77ac8
Reviewed-on: http://gerrit.causes.com/44730
Tested-by: jenkins <jenkins@brigade.com>
Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

lib/scss_lint/linter/duplicate_property.rb
@@ -3,15 +3,45 @@ module SCSSLint
   class Linter::DuplicateProperty < Linter
     include LinterRegistry
 
-    def visit_rule(node)
-      check_properties(node)
+    def check_properties(node)
+      static_properties(node).each_with_object({}) do |prop, prop_names|
+
+        prop_key = property_key(prop)
+
+        if existing_prop = prop_names[prop_key]
+          add_lint(prop, "Property `#{name}` already defined on line #{existing_prop.line}")
+        else
+          prop_names[prop_key] = prop
+        end
+      end
+
+      yield # Continue linting children
     end
 
-    def visit_mixindef(node)
-      check_properties(node)
-    end
+    alias_method :visit_rule,     :check_properties
+    alias_method :visit_mixindef, :check_properties
 
   private
+
+    def static_properties(node)
+      node.children
+          .select { |child| child.is_a?(Sass::Tree::PropNode) }
+          .reject { |prop| prop.name.any? { |item| item.is_a?(Sass::Script::Node) } }
+    end
+
+    # Returns a key identifying the bucket this property and value correspond to
+    # for purposes of uniqueness.
+    def property_key(prop)
+      prop_key = prop.name.join
+      prop_value = property_value(prop)
+
+      # Differentiate between values for different vendor prefixes
+      prop_value.to_s.scan(/^(-[^-]+-.+)/) do |vendor_keyword|
+        prop_key << vendor_keyword.first
+      end
+
+      prop_key
+    end
 
     def property_value(prop)
       case prop.value
@@ -22,31 +52,6 @@ module SCSSLint
         prop.value.value
       else
         prop.value.to_s
-      end
-    end
-
-    def check_properties(node)
-      properties = node.children
-                       .select { |child| child.is_a?(Sass::Tree::PropNode) }
-                       .reject { |prop| prop.name.any? { |item| item.is_a?(Sass::Script::Node) } }
-
-      prop_names = {}
-
-      properties.each do |prop|
-        name = prop.name.join
-
-        prop_hash = name
-        prop_value = property_value(prop)
-
-        prop_value.to_s.scan(/^(-[^-]+-.+)/) do |vendor_keyword|
-          prop_hash << vendor_keyword.first
-        end
-
-        if existing_prop = prop_names[prop_hash]
-          add_lint(prop, "Property `#{name}` already defined on line #{existing_prop.line}")
-        else
-          prop_names[prop_hash] = prop
-        end
       end
     end
   end

spec/scss_lint/linter/duplicate_property_spec.rb
@@ -173,4 +173,17 @@ describe SCSSLint::Linter::DuplicateProperty do
 
     it { should report_lint line: 3 }
   end
+
+  context 'when property contains a duplicate value in a nested rule set' do
+    let(:css) { <<-CSS }
+      .outer {
+        .inner {
+          color: $some-color;
+          color: $some-color;
+        }
+      }
+    CSS
+
+    it { should report_lint line: 4 }
+  end
 end

commit 4b336a2f1cb1b0deb1536d932adce34fb62e26d0

Fix name reported by DuplicateProperty linter
This was broken by the refactoring in 4fd093c3.

Change-Id: Iebc1ed465acd499d2163e9e17e0e6728fbcd3e23
Reviewed-on: http://gerrit.causes.com/44935
Tested-by: jenkins <jenkins@brigade.com>
Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

lib/scss_lint/linter/duplicate_property.rb
@@ -5,11 +5,11 @@ module SCSSLint
 
     def check_properties(node)
       static_properties(node).each_with_object({}) do |prop, prop_names|
-
         prop_key = property_key(prop)
 
         if existing_prop = prop_names[prop_key]
-          add_lint(prop, "Property `#{name}` already defined on line #{existing_prop.line}")
+          add_lint(prop, "Property `#{existing_prop.name.join}` already "\
+                         "defined on line #{existing_prop.line}")
         else
           prop_names[prop_key] = prop
         end