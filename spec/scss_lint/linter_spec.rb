8b80a8ea675ab7d5c0b7060618241eea26786b0b
Michael Bertolacci <m.bertolacci@gmail.com>
2014-12-01 13:34:17 +1100

    Use a single linter to test control comments to simplify the test

--- spec/scss_lint/linter_spec.rb
+++ spec/scss_lint/linter_spec.rb
@@ -2,29 +2,14 @@ require 'spec_helper'
 
 describe SCSSLint::Linter do
   describe 'control comments' do
-    before do
-      @linter1 = SCSSLint::Linter::Fake1.new
-      @linter2 = SCSSLint::Linter::Fake2.new
-
-      initial_indent = css[/\A(\s*)/, 1]
-      normalized_css = css.gsub(/^#{initial_indent}/, '')
-
-      @linter1.run(SCSSLint::Engine.new(normalized_css), {})
-      @linter2.run(SCSSLint::Engine.new(normalized_css), {})
-    end
+    let(:linter_config) { {} }
+    subject             { SCSSLint::Linter::Fake.new }
 
     module SCSSLint
-      class Linter::Fake1 < SCSSLint::Linter
+      class Linter::Fake < SCSSLint::Linter
         def visit_prop(node)
-          return unless node.value.to_sass.strip == 'fail1'
+          return unless node.value.to_sass.strip == 'fail'
           add_lint(node, 'everything offends me')
-        end
-      end
-
-      class Linter::Fake2 < SCSSLint::Linter
-        def visit_prop(node)
-          return unless node.value.to_sass.strip == 'fail2'
-          add_lint(node, 'everything offends me too')
         end
       end
     end
@@ -32,185 +17,178 @@ describe SCSSLint::Linter do
     context 'when a disable is not present' do
       let(:css) { <<-CSS }
         p {
-          border: fail1;
+          border: fail;
 
           a {
-            border: fail1;
+            border: fail;
           }
         }
       CSS
 
-      it { @linter1.should report_lint }
+      it { should report_lint }
     end
 
     context 'when a disable is present at the top level' do
       let(:css) { <<-CSS }
-        // scss-lint:disable Fake1
+        // scss-lint:disable Fake
         p {
-          border: fail1;
+          border: fail;
 
           a {
-            border: fail1;
+            border: fail;
           }
         }
       CSS
 
-      it { @linter1.should_not report_lint }
+      it { should_not report_lint }
     end
 
-    context 'when a disable is present at the top level for one linter but not the other' do
+    context 'when a disable is present at the top level for another linter' do
       let(:css) { <<-CSS }
-        // scss-lint:disable Fake1
+        // scss-lint:disable Bogus
         p {
-          border: fail1;
+          border: fail;
         }
         p {
-          border: fail2;
+          border: bogus;
         }
       CSS
 
-      it { @linter1.should_not report_lint }
-      it { @linter2.should report_lint }
+      it { should report_lint lint: 3 }
     end
 
     context 'when a linter is disabled then enabled again' do
       let(:css) { <<-CSS }
-        // scss-lint:disable Fake1
+        // scss-lint:disable Fake
         p {
-          border: fail1;
+          border: fail;
         }
-        // scss-lint:enable Fake1
+        // scss-lint:enable Fake
         p {
-          border: fail1;
+          border: fail;
         }
       CSS
 
-      it { @linter1.should_not report_lint line: 3 }
-      it { @linter1.should report_lint line: 7 }
+      it { should_not report_lint line: 3 }
+      it { should report_lint line: 7 }
     end
 
     context 'when a linter is disabled within a rule' do
       let(:css) { <<-CSS }
         p {
-          // scss-lint:disable Fake1
-          border: fail1;
+          // scss-lint:disable Fake
+          border: fail;
 
           a {
-            border: fail1;
+            border: fail;
           }
         }
 
         p {
-          border: fail1;
+          border: fail;
         }
       CSS
 
-      it { @linter1.should_not report_lint line: 3 }
-      it { @linter1.should_not report_lint line: 6 }
-      it { @linter1.should report_lint line: 11 }
+      it { should_not report_lint line: 3 }
+      it { should_not report_lint line: 6 }
+      it { should report_lint line: 11 }
     end
 
     context 'when more than one linter is disabled' do
       let(:css) { <<-CSS }
-        // scss-lint:disable Fake1, Fake2
+        // scss-lint:disable Bogus, Fake
         p {
-          border: fail1;
+          border: fail;
         }
 
         p {
-          border: fail2;
+          border: bogus;
         }
       CSS
 
-      it { @linter1.should_not report_lint }
-      it { @linter2.should_not report_lint }
+      it { should_not report_lint }
     end
 
     context 'when more than one linter is disabled without spaces between the linter names' do
       let(:css) { <<-CSS }
-        // scss-lint:disable Fake1,Fake2
+        // scss-lint:disable Fake,Bogus
         p {
-          border: fail1;
+          border: fail;
         }
 
         p {
-          border: fail2;
+          border: Bogus;
         }
       CSS
 
-      it { @linter1.should_not report_lint }
-      it { @linter2.should_not report_lint }
+      it { should_not report_lint }
     end
 
     context 'when more than one linter is disabled without commas between the linter names' do
       let(:css) { <<-CSS }
-        // scss-lint:disable Fake1 Fake2
+        // scss-lint:disable Fake Bogus
         p {
-          border: fail1;
+          border: fail;
         }
 
         p {
-          border: fail2;
+          border: bogus;
         }
       CSS
 
-      it { @linter1.should_not report_lint }
-      it { @linter2.should_not report_lint }
+      it { should_not report_lint }
     end
 
     context 'when two linters are disabled and only one is reenabled' do
       let(:css) { <<-CSS }
-        // scss-lint:disable Fake1, Fake2
+        // scss-lint:disable Fake, Bogus
         p {
-          border: fail1;
+          border: fail;
         }
-        // scss-lint:enable Fake1
+        // scss-lint:enable Fake
 
         p {
-          margin: fail1;
-          border: fail2;
+          margin: fail;
+          border: bogus;
         }
       CSS
 
-      it { @linter1.should_not report_lint line: 3 }
-      it { @linter2.should_not report_lint }
-      it { @linter1.should report_lint line: 8 }
+      it { should_not report_lint line: 3 }
+      it { should report_lint line: 8 }
     end
 
     context 'when all linters are disabled' do
       let(:css) { <<-CSS }
         // scss-lint:disable all
         p {
-          border: fail1;
+          border: fail;
         }
 
         p {
-          margin: fail1;
-          border: fail2;
+          margin: fail;
+          border: bogus;
         }
       CSS
 
-      it { @linter1.should_not report_lint }
-      it { @linter2.should_not report_lint }
+      it { should_not report_lint }
     end
 
     context 'when all linters are disabled and then one is re-enabled' do
       let(:css) { <<-CSS }
         // scss-lint:disable all
         p {
-          border: fail1;
+          border: fail;
         }
-        // scss-lint:enable Fake1
+        // scss-lint:enable Fake
 
         p {
-          margin: fail1;
-          border: fail2;
+          margin: fail;
+          border: bogus;
         }
       CSS
 
-      it { @linter1.should_not report_lint line: 3 }
-      it { @linter2.should_not report_lint }
-      it { @linter1.should report_lint line: 8 }
+      it { should_not report_lint line: 3 }
+      it { should report_lint line: 8 }
     end
   end
 end
