8c00d1be6adcbf1a5c09a6a6f1c4d2bd83bfe9d9
Shane da Silva <shane@dasilva.io>
2014-12-01 21:27:59 -0800

    Clean up ControlComments module
    Convert some of the Ruby in this file to be more idiomatic.
    
    Change-Id: Icd7ec38f422d4dccba9edbcfcd4f25e1fe4bb89c
    Reviewed-on: http://gerrit.causes.com/44952
    Tested-by: jenkins <jenkins@brigade.com>
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

--- lib/scss_lint/control_comments.rb
+++ lib/scss_lint/control_comments.rb
@@ -1,8 +1,8 @@
-module SCSSLint
-  # Adds control comment functionality
-  module ControlComments
-    attr_reader :disable_stack, :disabled_lines
+require 'set'
 
+module SCSSLint
+  # Contains functionality for enabling/disabling linters via comments.
+  module ControlComments
   private
 
     def initialize_vars
@@ -13,52 +13,60 @@ module SCSSLint
     def filter_lints
       initialize_vars
 
-      @lints = lints.select { |lint| !disabled_lines.include?(lint.location.line) }
+      @lints = lints.reject { |lint| @disabled_lines.include?(lint.location.line) }
     end
 
-    def enter_visit_control_comment(node)  # rubocop:disable CyclomaticComplexity
+    def enter_visit_control_comment(node)
       return unless node.is_a?(Sass::Tree::CommentNode)
 
+      return unless match = %r{
+        /\*\s* # Comment start marker
+        scss-lint:
+        (?<command>disable|enable)\s+
+        (?<linters>.*?)
+        \s*\*/ # Comment end marker
+      }x.match(node.value.first)
+
+      linters = match[:linters].split(/\s*,\s*|\s+/)
+      return unless linters.include?('all') || linters.include?(name)
+
       initialize_vars
+      process_command(match[:command], node)
+    end
 
-      match = %r{/* scss\-lint:(disable|enable) (.*?) \*/}.match(node.value[0])
-      return if match.nil?
-
-      linters = match[2].split(/\s*,\s*|\s+/)
-      return unless match[2] == 'all' || linters.include?(name)
-
-      case match[1]
-      when 'disable' then @disable_stack << node
-      when 'enable' then pop_control_comment_stack(node)
+    def process_command(command, node)
+      case command
+      when 'disable'
+        @disable_stack << node
+      when 'enable'
+        pop_control_comment_stack(node)
       end
     end
 
     def exit_visit_control_comment(node)
       initialize_vars
 
-      while @disable_stack.last && @disable_stack.last.node_parent == node
+      while @disable_stack.any? && @disable_stack.last.node_parent == node
         pop_control_comment_stack(node)
       end
     end
 
     def pop_control_comment_stack(node)
-      comment_node = @disable_stack.pop
-
-      return unless comment_node
+      return unless comment_node = @disable_stack.pop
 
       start_line = comment_node.line
 
-      # Find the deepest child that has a line number to which a lint might apply (if it is a
-      # control comment enabe node, it will be the line of the comment itself)
+      # Find the deepest child that has a line number to which a lint might
+      # apply (if it is a control comment enable node, it will be the line of
+      # the comment itself).
       child = node
-      loop do
-        break unless child.children.last.is_a?(Sass::Tree::Node)
+      while child.children.last.is_a?(Sass::Tree::Node)
         child = child.children.last
       end
 
       end_line = child.line
 
-      disabled_lines.merge((start_line..end_line))
+      @disabled_lines.merge(start_line..end_line)
     end
   end
 end
