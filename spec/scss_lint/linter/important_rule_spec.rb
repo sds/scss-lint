208252468e16325ef883f203e4c7f95bcdfd2310
Andrew Grauch and Mike Holm <agrauch@rallydev.com>
2015-01-22 14:18:14 -0700

    Add linter for important rule

---
+++ spec/scss_lint/linter/important_rule_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe SCSSLint::Linter::ImportantRule do
+  context 'when no !important is used' do
+    let(:css) { <<-CSS }
+      p {
+        color: #000;
+      }
+    CSS
+
+    it { should_not report_lint }
+  end
+
+  context 'when !important is used' do
+    let(:css) { <<-CSS }
+      p {
+        color: #000!important;
+      }
+    CSS
+
+    it { should report_lint line: 2 }
+  end
+end
