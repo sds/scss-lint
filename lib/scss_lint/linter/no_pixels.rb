1eb3cb802cb52c4a783d6dccb037e14f13bf03ff
Ian McNally <imcnally@stridenyc.com>
2015-03-31 17:45:50 -0400

    allow ignored properties for no pixels linting

--- lib/scss_lint/linter/no_pixels.rb
+++ lib/scss_lint/linter/no_pixels.rb
@@ -5,6 +5,8 @@ module SCSSLint
 
     def visit_prop(node)
       return unless node.value.value.to_s.include? 'px'
+      ignored_properties = config['ignored_properties'].to_set
+      return if ignored_properties.include? node.name.join
       add_lint(node, 'Pixel units should not be used')
     end
 
--- spec/scss_lint/linter/no_pixels_spec.rb
+++ spec/scss_lint/linter/no_pixels_spec.rb
@@ -32,7 +32,7 @@ describe SCSSLint::Linter::NoPixels do
     it { should report_lint line: 2 }
   end
 
-    context 'when non-pixel and pixel value are used on same selector' do
+  context 'when non-pixel and pixel value are used on same selector' do
     let(:scss) { <<-SCSS }
       p {
         margin: 1.6px;
@@ -44,4 +44,40 @@ describe SCSSLint::Linter::NoPixels do
     it { should_not report_lint line: 3 }
   end
 
+  context 'when properties are ignored' do
+    let(:linter_config) { { 'ignored_properties' => ['font-size'] } }
+
+    context 'when property is ignored and using pixels' do
+      let(:scss) { <<-SCSS }
+        p {
+          font-size: 16px;
+        }
+      SCSS
+
+      it { should_not report_lint }
+    end
+
+    context 'when property is not ignored and using pixels' do
+      let(:scss) { <<-SCSS }
+        p {
+          margin: 1px;
+        }
+      SCSS
+
+      it { should report_lint line: 2 }
+    end
+
+    context 'when mixing ignored and not ignored properties using pixels' do
+      let(:scss) { <<-SCSS }
+        p {
+          font-size: 16px;
+          margin: 1px;
+        }
+      SCSS
+
+      it { should_not report_lint line: 2 }
+      it { should report_lint line: 3 }
+    end
+
+  end
 end
