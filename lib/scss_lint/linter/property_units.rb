69d56bf73050421ce8314e2bcddfcd5ac72d3b9c
Joe Lencioni <joe.lencioni@brigade.com>
2015-06-20 12:21:17 -0700

    Fix `PropertyUnits` for multiple unit-line values
    I noticed that this linter didn't correctly check all of the unit-like
    parts of the value, which can happen pretty easily if you are using
    shorthand values. For example:
    
      margin: 1px 2px 3pix 4px;
    
    would have not been caught previously whereas:
    
      margin: 1pix 2px 3px 4px;
    
    would have. I fixed this by finding all of the unit-like parts of the
    value and checking the units on each of them. I moved the regex up to a
    constant to avoid Rubocop's too many lines in method warning.

--- lib/scss_lint/linter/property_units.rb
+++ lib/scss_lint/linter/property_units.rb
@@ -3,6 +3,16 @@ module SCSSLint
   class Linter::PropertyUnits < Linter
     include LinterRegistry
 
+    NUMBER_WITH_UNITS_REGEX = /
+      (?:^|\s)    # beginning of value or whitespace
+      (?:
+        \d+       # any number of digits, e.g. 123
+        |         # or
+        \d*\.?\d+ # any number of digits with decimal, e.g. 1.23 or .123
+      )
+      ([a-z%]+)   # letters or percent sign, e.g. px or %
+    /ix
+
     def visit_root(_node)
       @globally_allowed_units = config['global'].to_set
       @allowed_units_for_property = config['properties']
@@ -18,19 +28,10 @@ module SCSSLint
         property = "#{@nested_under}-#{property}"
       end
 
-      number_with_units_regex = /
-        (?:^|\s)    # beginning of value or whitespace
-        (?:
-          \d+       # any number of digits, e.g. 123
-          |         # or
-          \d*\.?\d+ # any number of digits with decimal, e.g. 1.23 or .123
-        )
-        ([a-z%]+)   # letters or percent sign, e.g. px or %
-      /ix
-
-      if node.value.respond_to?(:value) &&
-        units = node.value.value.to_s[number_with_units_regex, 1]
-        check_units(node, property, units)
+      if node.value.respond_to?(:value)
+        node.value.value.to_s.scan(NUMBER_WITH_UNITS_REGEX).each do |units|
+          check_units(node, property, units[0])
+        end
       end
 
       @nested_under = property
