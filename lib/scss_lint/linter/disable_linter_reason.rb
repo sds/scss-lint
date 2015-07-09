a573a65c3dc0252d0ca9738b75cb7f564dce527d
Sam Rawlins <srawlins@google.com>
2015-07-09 12:53:25 -0700

    New linter: DisableLinterReason

---
+++ lib/scss_lint/linter/disable_linter_reason.rb
@@ -0,0 +1,39 @@
+module SCSSLint
+  # Checks for "reason" comments above linter-disabling comments.
+  class Linter::DisableLinterReason < Linter
+    include LinterRegistry
+
+    def visit_comment(node)
+      return unless node.value.first.match(COMMAND_REGEX)
+
+      # No lint if the first line of the comment is not the command.
+      return unless comment_lines(node).first.match(COMMAND_REGEX)
+
+      # Maybe the previous node is the "reason" comment.
+      prev = previous_node(node)
+
+      if prev && prev.is_a?(Sass::Tree::CommentNode)
+        # No lint if the last line of the previous comment is not a command.
+        return unless comment_lines(prev).last.match(COMMAND_REGEX)
+      end
+
+      add_lint(node,
+               'scss-lint:disable control comments should be preceded by a ' \
+               'comment explaining why the linters need to be disabled.')
+    end
+
+    private
+
+    COMMAND_REGEX = %r{
+      (/|\*)\s* # Comment start marker
+      scss-lint:
+      (?<action>disable|enable)\s+
+      (?<linters>.*?)
+      \s*(?:\*/|\n) # Comment end marker or end of line
+    }x
+
+    def comment_lines(node)
+      node.value.join.split("\n")
+    end
+  end
+end
