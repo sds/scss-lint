906be395d9e412783f83e0d2724e6674722b715e
Sam Rawlins <srawlins@google.com>
2015-08-31 10:10:23 -0700

    Support any command line flags in the rake task

--- spec/scss_lint/rake_task_spec.rb
+++ spec/scss_lint/rake_task_spec.rb
@@ -3,12 +3,12 @@ require 'scss_lint/rake_task'
 require 'tempfile'
 
 describe SCSSLint::RakeTask do
-  before(:all) do
-    SCSSLint::RakeTask.new
-  end
-
   before do
     STDOUT.stub(:write) # Silence console output
+  end
+
+  after(:each) do
+    Rake::Task['scss_lint'].clear if Rake::Task.task_defined?('scss_lint')
   end
 
   let(:file) do
@@ -25,19 +25,68 @@ describe SCSSLint::RakeTask do
     end
   end
 
-  context 'when SCSS document is valid with no lints' do
+  context 'basic RakeTask' do
+    before(:each) do
+      SCSSLint::RakeTask.new
+    end
+
+    context 'when SCSS document is valid with no lints' do
+      let(:scss) { '' }
+
+      it 'does not call Kernel.exit' do
+        expect { run_task }.not_to raise_error
+      end
+    end
+
+    context 'when SCSS document is invalid' do
+      let(:scss) { '.class {' }
+
+      it 'calls Kernel.exit with the status code' do
+        expect { run_task }.to raise_error SystemExit
+      end
+    end
+  end
+
+  context 'configured RakeTask with a config file' do
     let(:scss) { '' }
 
-    it 'does not call Kernel.exit' do
+    let(:config_file) do
+      config = Tempfile.new(%w[foo .yml])
+      config.write('')
+      config.close
+      config.path
+    end
+
+    it 'passes config files to the CLI' do
+      SCSSLint::RakeTask.new.tap do |t|
+        t.config = config_file
+      end
+
+      cli = double(SCSSLint::CLI)
+      SCSSLint::CLI.should_receive(:new) { cli }
+      args = ['--config', config_file, file.path]
+      cli.should_receive(:run).with(args) { 0 }
+
       expect { run_task }.not_to raise_error
     end
   end
 
-  context 'when SCSS document is invalid' do
-    let(:scss) { '.class {' }
+  context 'configured RakeTask with args' do
+    let(:scss) { '' }
 
-    it 'calls Kernel.exit with the status code' do
-      expect { run_task }.to raise_error SystemExit
+    it 'passes args to the CLI' do
+      formatter_args = ['--formatter', 'JSON']
+
+      SCSSLint::RakeTask.new.tap do |t|
+        t.args = formatter_args
+      end
+
+      cli = double(SCSSLint::CLI)
+      SCSSLint::CLI.should_receive(:new) { cli }
+      args = formatter_args + [file.path]
+      cli.should_receive(:run).with(args) { 0 }
+
+      expect { run_task }.not_to raise_error
     end
   end
 end
