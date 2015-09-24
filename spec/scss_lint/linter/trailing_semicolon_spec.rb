sds/scss-lint

commit e29cc409979d3025b1f43dde36cd169cdc1250da

Fix TrailingSemicolon handling of trailing comments
When a semicolon appeared at the end of a trailing comment, i.e.

    $foo: bar // comment;

...`TrailingSemicolon` would not report an error. This was because we
were looking at the `VariableNode`'s source range and not its underlying
expression.

lib/scss_lint/linter/trailing_semicolon.rb
@@ -8,7 +8,7 @@ module SCSSLint
     end
 
     def visit_variable(node)
-      check_semicolon(node)
+      check_semicolon(node.expr)
     end
 
     def visit_prop(node)

spec/scss_lint/linter/trailing_semicolon_spec.rb
@@ -301,4 +301,10 @@ describe SCSSLint::Linter::TrailingSemicolon do
 
     it { should_not report_lint }
   end
+
+  context 'when variable declaration is followed by a comment and semicolon' do
+    let(:scss) { '$foo: bar // comment;' }
+
+    it { should report_lint }
+  end
 end

commit d5cee65752ac0b3f4b2e9dae0c68040ba8686c20

Fix TrailingSemicolon to handle parenthesized expressions
Commit e29cc40 introduced a bug where TrailingSemicolon would
erroneously report errors for parenthesized expressions, i.e.

    $foo: ($bar);

Fix it by adding some special cases.

lib/scss_lint/linter/trailing_semicolon.rb
@@ -52,12 +52,26 @@ module SCSSLint
 
     # Checks that the node is ended by a semicolon (with no whitespace)
     def ends_with_semicolon?(node)
-      source_from_range(node.source_range) =~ /;(\s*})?$/
+      semicolon_after_parenthesis?(node) ||
+        # Otherwise just check for a semicolon
+        source_from_range(node.source_range) =~ /;(\s*})?$/
+    end
+
+    # Special case: Sass doesn't include the semicolon after an expression
+    # in the source range it reports, so we need a helper to check after the
+    # reported range.
+    def semicolon_after_parenthesis?(node)
+      last_char = character_at(node.source_range.end_pos)
+      char_after = character_at(node.source_range.end_pos, 1)
+      (last_char == ')' && char_after == ';') ||
+        ([last_char, char_after].include?("\n") &&
+         engine.lines[node.source_range.end_pos.line] =~ /\);(\s*})?$/)
     end
 
     def ends_with_multiple_semicolons?(node)
       # Look one character past the end to see if there's another semicolon
-      character_at(node.source_range.end_pos, 1) == ';'
+      character_at(node.source_range.end_pos) == ';' &&
+        character_at(node.source_range.end_pos, 1) == ';'
     end
 
     def has_space_before_semicolon?(node)

spec/scss_lint/linter/trailing_semicolon_spec.rb
@@ -307,4 +307,48 @@ describe SCSSLint::Linter::TrailingSemicolon do
 
     it { should report_lint }
   end
+
+  context 'when a variable declaration contains parentheses' do
+    context 'and ends with a semicolon' do
+      let(:scss) { '$foo: ($expr);' }
+
+      it { should_not report_lint }
+    end
+
+    context 'and is missing a semicolon' do
+      let(:scss) { '$foo: ($expr)' }
+
+      it { should report_lint }
+    end
+  end
+
+  context 'when a variable declaration contains a multiline map' do
+    context 'with a trailing comma' do
+      context 'and ends with a semicolon' do
+        let(:scss) { "$foo: (\n  one: 1,\ntwo: 2,\n);" }
+
+        it { should_not report_lint }
+      end
+
+      context 'and is missing a semicolon' do
+        let(:scss) { "$foo: (\n  one: 1,\ntwo: 2,\n)" }
+
+        it { should report_lint }
+      end
+    end
+
+    context 'without a trailing comma' do
+      context 'and ends with a semicolon' do
+        let(:scss) { "$foo: (\n  one: 1,\ntwo: 2\n);" }
+
+        it { should_not report_lint }
+      end
+
+      context 'and is missing a semicolon' do
+        let(:scss) { "$foo: (\n  one: 1,\ntwo: 2\n)" }
+
+        it { should report_lint }
+      end
+    end
+  end
 end