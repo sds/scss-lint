3841c27ad45c699e0ca7f245fbb677cb4df31b69
Joe Lencioni <joe.lencioni@airbnb.com>
2016-02-22 12:30:37 -0800

    Use class names in PrivateNamingConvention
    Instead of passing around some arbitrary strings, we can just use the
    class names here. I think this simplifies some things and will make my
    next change that intends to fix an edge case a little nicer.

--- lib/scss_lint/linter/private_naming_convention.rb
+++ lib/scss_lint/linter/private_naming_convention.rb
@@ -4,16 +4,17 @@ module SCSSLint
   class Linter::PrivateNamingConvention < Linter
     include LinterRegistry
 
+    DEFINING_CLASSES = [
+      Sass::Tree::FunctionNode,
+      Sass::Tree::MixinDefNode,
+      Sass::Tree::VariableNode,
+    ].freeze
+
     def visit_root(node)
       # Register all top-level function, mixin, and variable definitions.
       node.children.each_with_object([]) do |child_node|
-        case child_node
-        when Sass::Tree::FunctionNode
-          register_node child_node, 'function'
-        when Sass::Tree::MixinDefNode
-          register_node child_node, 'mixin'
-        when Sass::Tree::VariableNode
-          register_node child_node, 'variable'
+        if DEFINING_CLASSES.include?(child_node.class)
+          register_node child_node
         else
           yield
         end
@@ -25,43 +26,44 @@ module SCSSLint
     end
 
     def visit_script_funcall(node)
-      check_privacy(node, 'function')
+      check_privacy(node, Sass::Tree::FunctionNode)
       yield # Continue linting any arguments of this function call
     end
 
     def visit_mixin(node)
-      check_privacy(node, 'mixin')
+      check_privacy(node, Sass::Tree::MixinDefNode)
       yield # Continue into content block of this mixin's block
     end
 
     def visit_script_variable(node)
-      check_privacy(node, 'variable')
+      check_privacy(node, Sass::Tree::VariableNode)
     end
 
   private
 
-    def register_node(node, node_type, node_text = node.name)
+    def register_node(node, node_text = node.name)
       return unless private?(node)
 
       @private_definitions ||= {}
-      @private_definitions[node_type] ||= {}
+      @private_definitions[node.class.name] ||= {}
 
-      @private_definitions[node_type][node_text] = {
+      @private_definitions[node.class.name][node_text] = {
         node: node,
         times_used: 0,
       }
     end
 
-    def check_privacy(node, node_type, node_text = node.name)
+    def check_privacy(node, defining_node_class, node_text = node.name)
       return unless private?(node)
 
       if @private_definitions &&
-          @private_definitions[node_type] &&
-          @private_definitions[node_type][node_text]
-        @private_definitions[node_type][node_text][:times_used] += 1
+          @private_definitions[defining_node_class.name] &&
+          @private_definitions[defining_node_class.name][node_text]
+        @private_definitions[defining_node_class.name][node_text][:times_used] += 1
         return
       end
 
+      node_type = humanize_node_class(node)
       add_lint(
         node, "Private #{node_type} #{node_text} must be defined in the same file it is used")
     end
@@ -76,11 +78,23 @@ module SCSSLint
       @private_definitions.each do |node_type, nodes|
         nodes.each do |node_text, node_info|
           next if node_info[:times_used] > 0
+          node_type = humanize_node_class(node_info[:node])
           add_lint(
             node_info[:node],
             "Private #{node_type} #{node_text} must be used in the same file it is defined"
           )
         end
+      end
+    end
+
+    def humanize_node_class(node)
+      case node
+      when Sass::Tree::FunctionNode
+        'function'
+      when Sass::Tree::MixinDefNode
+        'mixin'
+      when Sass::Tree::VariableNode
+        'variable'
       end
     end
   end
