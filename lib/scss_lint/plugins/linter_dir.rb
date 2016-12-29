787c381e047bc9bb8cf9dce33b5c7b3e57d58dfa
Remi Barraquand <dev@remibarraquand.com>
2016-12-29 18:19:00 +0100

    Allow loading config file from plugin directory
    `plugin_directories` property can be used in `.scss-linter.yml` to
    load additional linters from a directory. However it does not allow
    loading custom `.scss-linter.yml` for such directory.
    
    This commit allows loading a custom `.scss-linter.yml` from any
    directory specified in `plugin_directories` and merge it with
    your existing configuration.

--- lib/scss_lint/plugins/linter_dir.rb
+++ lib/scss_lint/plugins/linter_dir.rb
@@ -6,11 +6,11 @@ module SCSSLint
 
       def initialize(dir)
         @dir = dir
-        @config = SCSSLint::Config.new({}) # Will always be empty
       end
 
       def load
         ruby_files.each { |file| require file }
+        @config = plugin_config
         self
       end
 
@@ -18,6 +18,29 @@ module SCSSLint
 
       def ruby_files
         Dir.glob(File.expand_path(File.join(@dir, '**', '*.rb')))
+      end
+
+      # Returns the {SCSSLint::Config} for this directory.
+      #
+      # This is intended to be merged with the configuration that loaded this
+      # plugin.
+      #
+      # @return [SCSSLint::Config]
+      def plugin_config
+        file = plugin_config_file
+
+        if File.exist?(file)
+          Config.load(file, merge_with_default: false)
+        else
+          Config.new({})
+        end
+      end
+
+      # Path of the configuration file to attempt to load for this directory.
+      #
+      # @return [String]
+      def plugin_config_file
+        File.join(@dir, Config::FILE_NAME)
       end
     end
   end
