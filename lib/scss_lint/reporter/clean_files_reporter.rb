b36f785adff186b9407d704547d09092eda48fbe
Sam Rawlins <srawlins@google.com>
2018-01-11 22:34:12 -0800

    Fix CleanFilesReporter

--- lib/scss_lint/reporter/clean_files_reporter.rb
+++ lib/scss_lint/reporter/clean_files_reporter.rb
@@ -3,7 +3,7 @@ module SCSSLint
   class Reporter::CleanFilesReporter < Reporter
     def report_lints
       dirty_files = lints.map(&:filename).uniq
-      clean_files = files - dirty_files
+      clean_files = files.map { |e| e['path'] } - dirty_files
       clean_files.sort.join("\n") + "\n" if clean_files.any?
     end
   end
